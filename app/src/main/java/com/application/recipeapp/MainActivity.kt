646d32beb825ca70dc165c96f071257b4ab97c0d
Joel Oguntoye <joeloguntoye@gmail.com>
2021-01-25 13:57:03 +0100

    Working with row layouts in jetPack Compose

--- app/src/main/java/com/application/recipeapp/MainActivity.kt
+++ app/src/main/java/com/application/recipeapp/MainActivity.kt
@@ -2,18 +2,19 @@ package com.application.recipeapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.ScrollableColumn
-import androidx.compose.foundation.background
+import android.util.ArrayMap
+import androidx.compose.foundation.*
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.Button
 import androidx.compose.material.Text
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.imageFromResource
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.setContent
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.input.textInputServiceFactory
 import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.dp
 
@@ -21,6 +22,40 @@ import androidx.compose.ui.unit.dp
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+//        setContent {
+//            Column(
+//                    modifier =
+//                    Modifier.fillMaxWidth()
+//            ){
+//            Column(
+//                modifier = Modifier
+//                    .fillMaxWidth()
+//                    .height(200.dp)
+//                    .border(border = BorderStroke(width = 1.dp, color = Color.Black)),
+//                    verticalArrangement = Arrangement.Center
+//            ) {
+//                Text(
+//                    text = "Text1",
+//                    modifier = Modifier.align(Alignment.CenterHorizontally) //in columns children
+//                                                                            //can only be aligned horizontally
+//                )
+//            }
+//            Spacer(modifier = Modifier.padding(20.dp))
+//
+//            Row(modifier = Modifier
+//                    .width(200.dp)
+//                    .height(200.dp)
+//                    .border(border = BorderStroke(width = 1.dp, color = Color.Black)),
+//                    horizontalArrangement = Arrangement.Center
+//            ){
+//                Text(
+//                     text = "ITEM2",
+//                modifier = Modifier.align(Alignment.CenterVertically)
+//                )
+//            }
+//        }
+//      }
         setContent{
             ScrollableColumn(
                 modifier = Modifier
@@ -39,12 +74,25 @@ class MainActivity : AppCompatActivity() {
                 Column(
                     modifier = Modifier.padding(16.dp)
                 ){
-                    Text(
-                        text = "Happy Meal",
-                        style = TextStyle(
-                            fontSize = TextUnit.Companion.Sp(26)
+                    Row(modifier = Modifier.fillMaxWidth(),
+                    horizontalArrangement = Arrangement.SpaceBetween
+                    ){
+                        Text(
+                                text = "Happy Meal",
+                                modifier = Modifier.align(Alignment.CenterVertically),
+                                style = TextStyle(
+                                        fontSize = TextUnit.Companion.Sp(26)
+                                )
                         )
-                    )
+                        Text(
+                                text = "$5.99",
+                                style = TextStyle(
+                                        color = Color(0xFF85bb65),
+                                        fontSize = TextUnit.Companion.Sp(17)
+                                ),
+                                modifier = Modifier.align(Alignment.CenterVertically)
+                        )
+                    }
                     Spacer(modifier = Modifier.padding(top = 10.dp))
                     Text(
                         text = "800 Calories",
@@ -53,17 +101,13 @@ class MainActivity : AppCompatActivity() {
                         )
                     )
                     Spacer(modifier = Modifier.padding(top = 10.dp))
-                    Text(
-                        text = "$5.99",
-                        style = TextStyle(
-                            color = Color.Blue,
-                            fontSize = TextUnit.Companion.Sp(17)
-                        )
-                    )
+
+                    Button(onClick = { /*TODO*/ },
+                    modifier = Modifier.align(Alignment.CenterHorizontally)) {
+                            Text(text = "Order Now")
+                    }
                 }
             }
-
-
-        }
+      }
     }
 }
