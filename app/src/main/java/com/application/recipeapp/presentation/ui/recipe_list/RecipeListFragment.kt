d22e571fa3db1c54b1e8d1a24942ab2873619713
Joel Oguntoye <joeloguntoye@gmail.com>
2021-02-15 04:09:07 +0100

    persisting data in viewModel using MutableState

--- app/src/main/java/com/application/recipeapp/presentation/ui/recipe_list/RecipeListFragment.kt
+++ app/src/main/java/com/application/recipeapp/presentation/ui/recipe_list/RecipeListFragment.kt
@@ -1,4 +1,70 @@
 package com.application.recipeapp.presentation.ui.recipe_list
 
-class RecipeListFragment {
+import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.activity.result.ActivityResultCallback
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.ActivityResultRegistry
+import androidx.activity.result.contract.ActivityResultContract
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Button
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.ComposeView
+import androidx.compose.material.Text
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.unit.TextUnit
+import androidx.compose.ui.unit.dp
+import androidx.fragment.app.Fragment
+import dagger.hilt.android.AndroidEntryPoint
+import androidx.fragment.app.viewModels
+
+@AndroidEntryPoint
+class RecipeListFragment:Fragment() {
+
+    private val viewModel: RecipeListViewModel by viewModels()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return ComposeView(requireContext()).apply {
+            setContent{
+
+                //recipeList  variable is a MutableState,it's optimized for jetpack compose
+                 val recipeList = viewModel.recipes.value
+
+                for(recipe in recipeList) {
+                   Log.d("TAG","Recipe: + $recipe.title")
+                }
+
+                Column(modifier = Modifier.padding(16.dp)) {
+                    Text(
+                        text = "RecipeList",
+                        style = TextStyle(
+                            fontSize = TextUnit.Companion.Sp(21)
+                        )
+                    )
+                    Spacer(modifier = Modifier.padding(10.dp))
+                    Button(onClick = { /*viewModel.newSearch()*/ })
+                    {
+                        Text(text = " Perform Search")
+                        
+                    }
+                    
+                }
+
+            }
+        }
+    }
+
 }
--- app/src/main/java/com/application/recipeapp/presentation/ui/recipe_list/RecipeListViewModel.kt
+++ app/src/main/java/com/application/recipeapp/presentation/ui/recipe_list/RecipeListViewModel.kt
@@ -1,9 +1,37 @@
 package com.application.recipeapp.presentation.ui.recipe_list
 
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
 import androidx.hilt.lifecycle.ViewModelInject
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.application.recipeapp.domain.model.Recipe
+import com.application.recipeapp.repository.IrecipeRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
+import dagger.hilt.android.scopes.ViewModelScoped
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+import javax.inject.Named
 
 
-class RecipeListViewModel: ViewModel() {
+class RecipeListViewModel
+    @ViewModelInject
+    constructor(
+        private  val recipeRepository: IrecipeRepository,
+        private @Named("auth_token") val token: String
+    ): ViewModel() {
+
+    val recipes: MutableState<List<Recipe>> = mutableStateOf(listOf())
+
+    init {
+        viewModelScope.launch{
+            val result =   recipeRepository.search(
+            token,
+            1,
+            "chicken"
+        )
+            recipes.value = result
+        }
+
+    }
 }
